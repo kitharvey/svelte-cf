f38c7a5f10d652c0d6822227f2ee996ed78babd1
Kit Harvey Caubalejo <kitharveycaubalejo@gmail.com>
2025-01-27 11:27:18 +0800

    error when migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
-
+	out: './drizzle/migrations',
 	dbCredentials: {
 		accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
 		databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
